f25c5ab8d5da391c3c9a4c0b263641d1c0cbfda0
Hwangsooneun <catalyst8849@gmail.com>
2021-09-15 18:11:36 +0900

    set api

---
+++ src/channels/channels.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
+
+@Controller('api/workspaces/:url/channels')
+export class ChannelsController {
+    @Get()
+    getAllChannels() {
+
+    }
+
+    @Post(':name')
+    createChannel() {
+
+    }
+
+    @Get(':name')
+    getSpecificChannel() {
+        
+    }
+
+    @Get(':name/chats')
+    getChats(@Query() query, @Param() param) {
+        console.log(query.perPage, query.page)
+        console.log(param.id, param.url)
+    }
+
+    @Post(':name/chats')
+    postChats(@Body() body) {
+
+    }
+
+    @Get(':name/members')
+    getAllMembers() {
+
+    }
+
+    @Post(':name/members')
+    inviteMembers() {}
+}
