b02225968b13002abe79f46b7e4e6131e18ded7c
Hwangsooneun <catalyst8849@gmail.com>
2021-10-03 15:21:17 +0900

    learn interceptor

---
+++ src/common/interceptors/undefinedToNull.interceptor.ts
@@ -0,0 +1,13 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class UndefinedToNullInterceptor implements NestInterceptor {
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Observable<any> | Promise<Observable<any>> {
+        return next.handle().pipe(map((data) => data === undefined ? null : data));
+    }
+}
