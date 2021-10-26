a6dacef606c3ec3bae38c80392084e32e2400d1e
Hwangsooneun <catalyst8849@gmail.com>
2021-10-26 15:20:55 +0900

    learn member

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,10 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
-import { UsersService } from './users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as ormconfig from '../ormconfig'
 import { Users } from './entities/Users';
 import { AuthModule } from './auth/auth.module';
-import { EventsModule } from './events/events.module';
-import { EventsGateway } from './events.gateway';
 
 @Module({
   imports: [
@@ -27,10 +24,9 @@ import { EventsGateway } from './events.gateway';
     DmsModule,
     TypeOrmModule.forRoot(ormconfig),
     TypeOrmModule.forFeature([Users]),
-    EventsModule
   ],
   controllers: [AppController],
-  providers: [AppService, UsersService, EventsGateway],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
